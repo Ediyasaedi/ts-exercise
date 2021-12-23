b41429f79d6b8858f10251dcc8665c93b0798a1e
Ediyasaedi <ediyasa2724@gmail.com>
2021-12-23 23:22:41 +0700

    CodePotato #3 react, redux, typescript

--- shopping-cart/src/Products/ProductForm.tsx
+++ shopping-cart/src/Products/ProductForm.tsx
@@ -1,16 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppDispatch } from "../store.hooks";
+import { addProduct, Product } from "./products.slice";
 
-export interface ProductFormProps {}
+const ProductForm: React.FC = () => {
+  const [product, setProduct] = useState<Product>({
+    id: "",
+    title: "",
+    price: 0,
+  });
 
-const ProductForm: React.FC<ProductFormProps> = ({}) => {
+  const dispatch = useAppDispatch();
+
+  const { id, title, price } = product;
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    setProduct((prev) => {
+      (prev as any)[name] = value;
+      const newValue = { ...prev };
+      return newValue;
+    });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(addProduct(product));
+    setProduct({
+      id: "",
+      title: "",
+      price: 0,
+    });
+  };
   return (
     <>
       <h2>Add game to the store</h2>
-      <form>
-        <input type="text" placeholder="Game title" name="title" />
-        <input type="number" placeholder="Price" name="price" />
-        <input type="text" placeholder="Id" name="id" />
-        <button>Add price </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Game title"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          name="price"
+          value={price}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Id"
+          name="id"
+          value={id}
+          onChange={handleChange}
+        />
+        <button type="submit">Add Product</button>
       </form>
     </>
   );
